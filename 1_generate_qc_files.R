a4f3b0201e849ffa0ac37f59390c0222cc87e4a3
ntorrie <ntorrie@perennia.ca>
2024-12-12 15:09:19 -0400

    generate qc files river deployments

--- 1_generate_qc_files.R
+++ 1_generate_qc_files.R
@@ -3,23 +3,21 @@ library(quarto)
 library(rmarkdown)
 library(sensorstrings)
 
+#note, if running entire county at once, HIL flags do not get applied. 
+#Re-run stations which require HIL flags separately. WIP
 path <- file.path("R:/data_branches/inland_water_quality/processed_data/deployment_data")
 
-county <- "guysborough"
+county <- "Annapolis"
 
 depls <- list.files(
   paste(path, county, "new", sep = "/"),
   pattern = ".rds",
   full.names = TRUE
 )
-depls <- depls[1]
 
-# depls <- list.files(
-#   paste(path, county, sep = "/"),
-#   pattern = ".rds",
-#   full.names = TRUE
-# )
-# depls <- depls[27]
+depls
+#set depls to one file in list if you only want to run script for one station
+#depls <- depls[2:6]
 
 # export html file for each county showing the flagged observations
 sapply(depls, function(x) {
@@ -34,16 +32,25 @@ sapply(depls, function(x) {
 
 })
 
+# extra code
 
-x <- depls[1]
-depl_file <- x
-quarto_render(
-  input = here("1_apply_qc_tests.qmd"),
-  output_file = paste0(
-    sub(".rds", "", sub(".*/", "", x, perl = TRUE)),
-    ".html"
-  ),
-  execute_params = list(county = county, depl_file = x))
+# depls <- list.files(
+#   paste(path, county, sep = "/"),
+#   pattern = ".rds",
+#   full.names = TRUE
+# )
+# depls <- depls[27]
+
+
+# x <- depls[1]
+# depl_file <- x
+# quarto_render(
+#   input = here("1_apply_qc_tests.qmd"),
+#   output_file = paste0(
+#     sub(".rds", "", sub(".*/", "", x, perl = TRUE)),
+#     ".html"
+#   ),
+#   execute_params = list(county = county, depl_file = x))
 
 
 
